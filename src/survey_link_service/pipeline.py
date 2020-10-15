161bdedd05a188b9bdcce8b811f8ee7d2d95ac20
Stijn-Depla <s.depla@live.nl>
2020-10-15 14:24:52 +0200

    Add imports of packages to test layer functioning

--- src/survey_link_service/pipeline.py
+++ src/survey_link_service/pipeline.py
@@ -3,6 +3,9 @@ Pipeline calling Castor API and requesting new survey link when activated
 """
 
 from typing import Tuple
+import castorapi as ca
+from dotenv import load_dotenv
+from os import environ
 
 
 def pipeline_serve_link() -> Tuple[int, str, str]:
