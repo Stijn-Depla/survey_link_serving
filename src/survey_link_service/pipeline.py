f4fbc83d8d1b74a3de9dec004dd3a49379ede551
Stijn-Depla <s.depla@live.nl>
2020-10-15 16:53:06 +0200

    Generate survey link with pipeline

--- src/survey_link_service/pipeline.py
+++ src/survey_link_service/pipeline.py
@@ -19,10 +19,43 @@ def pipeline_serve_link() -> Tuple[int, str, Union[str, None]]:
                                        data={'client_id': environ["CLIENT_ID"],
                                              'client_secret': environ["CLIENT_SECRET"],
                                              'grant_type': 'client_credentials'})
+        print(json.loads(response_token.text))
         i_token = json.loads(response_token.text)['access_token']
 
-        print(i_token)
-        return 200, "Produced a response token: " + i_token, "https://www.success.com/"
+        # Create new record
+        response_create_record = requests.post(
+            i_base_url + i_api_request_path + "/study/" + environ["STUDY_ID"] + "/record",
+            headers={
+                'Authorization': 'Bearer ' + i_token,
+                "accept": "application/hal+json",
+                "Content-Type": 'application/json'
+            },
+            data=json.dumps({"institute_id": environ['INSTITUTE_ID']})
+        )
+        print(json.loads(response_create_record.text))
+        i_record_id = json.loads(response_create_record.text)['id']
+
+        # Request survey link for record
+        response_request_link = requests.post(
+            i_base_url + i_api_request_path + "/study/" + environ["STUDY_ID"] + "/surveypackageinstance",
+            headers={
+                'Authorization': 'Bearer ' + i_token,
+                "accept": "application/hal+json",
+                "Content-Type": 'application/json'
+            },
+            data=json.dumps({
+                "institute_id": environ['INSTITUTE_ID'],
+                "survey_package_id": environ['SURVEY_PACKAGE_ID'],
+                "record_id": i_record_id,
+                "email_address": "a.l.depla@umcutrecht.nl",
+                "auto_send": False,
+                "auto_lock_on_finish": True
+            })
+        )
+        print(json.loads(response_request_link.text))
+        i_link = "https://data.castoredc.com/survey/" + json.loads(response_request_link.text)["survey_url_string"]
+
+        return 200, "Produced a link", i_link
     except Exception as e:
         print(e)
         return 500, "Pipeline error occurred: " + str(e), None
